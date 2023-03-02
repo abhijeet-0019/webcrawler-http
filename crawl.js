5ce2187b3736c0a03a2ae4d05e5e4b3622c7a76b
abhijeet <abhijeetsinghbhainser@gmail.com>
2023-03-02 18:40:24 +0530

    git urls functions implemented

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsfromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements){
+        if(linkElement.href.slice(0, 1) === '/'){
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            }
+            catch(err){
+                console.log(`error with absolute: ${err.message}`)
+            }
+        }else{
+            //absolute
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            }
+            catch(err){
+                console.log(`error with absolute: ${err.message}`)
+            }
+        }
+        // console.log("------------->",linkElement.href)
+    }
+    return urls
+}
+
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,5 +41,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,64 @@
-const {normalizeURL} = require('./crawl')
+const {normalizeURL, getURLsfromHTML} = require('./crawl')
 const {test, expect} = require('@jest/globals')
+
 
 test('normalizeURL', ()=>{
     const input = 'https://BLog.boot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+// remove protocol -> logic implemented
+// remove trailing slashes -> logic implemented
+// remove capitals -> URL constructor taking care
+// also working with http -> yes working, URL constructor already removing it
+
+
+test('getURLsfromHTML absolute', ()=>{
+    const input = `
+        <html>
+            <body>
+                <a href="https://blog.boot.dev">
+                    Boot.dev Blog
+                </a>
+            </body>
+        <html>
+    `
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(input, inputBaseURL)
+    const expected = ["https://blog.boot.dev/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML relative', ()=>{
+    const input = `
+        <html>
+            <body>
+                <a href="/path/">
+                    Boot.dev Blog
+                </a>
+            </body>
+        <html>
+    `
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(input, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML error', ()=>{
+    const input = `
+        <html>
+            <body>
+                <a href="invalid">
+                    Boot.dev Blog
+                </a>
+            </body>
+        <html>
+    `
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(input, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
